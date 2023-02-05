0f039efc3df73ee22a4de9ee8db843b946e43af6
JeanCarlosSC <jeancarlosodi@gmail.com>
2023-02-05 16:42:12 -0500

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,63 @@
+import javax.swing.JButton
+import javax.swing.JFrame
+
+val sleepTime = Event(Hour(21, 35), Hour(9, 15), "sleep")
+
+fun main() {
+    AppController(GUI())
+}
+
+class AppController(gui: GUI) {
+
+}
+
+class GUI: JFrame() {
+    init {
+        loadComponents()
+        loadProperties()
+    }
+    fun loadComponents() {
+        val btAddConstraint = JButton("Add constraint")
+        btAddConstraint.setBounds(32, 32, 128, 32)
+        add(btAddConstraint)
+
+        val btShowCalendar = JButton("Show calendar")
+        btShowCalendar.setBounds(192, 32, 128, 32)
+        btShowCalendar.addActionListener { showCalendar() }
+        add(btShowCalendar)
+    }
+    fun showCalendar() {
+        println(sleepTime)
+    }
+    fun loadProperties() {
+        setSize(1280, 720)
+        setLocationRelativeTo(null)
+        layout = null
+        defaultCloseOperation = EXIT_ON_CLOSE
+        isVisible = true
+    }
+}
+
+class Event(val startTime: Hour, val duration: Hour, val name: String) {
+    override fun toString(): String {
+        return "$name ($startTime - ${startTime.plus(duration)})"
+    }
+}
+
+class Hour(var hour: Int, var minutes: Int) {
+    init {
+        while(minutes > 59) {
+            minutes -= 60
+            hour++
+        }
+        while(hour > 23) {
+            hour -= 24
+        }
+    }
+    override fun toString(): String {
+        return "$hour:$minutes"
+    }
+}
+fun Hour.plus(duration: Hour): Hour {
+    return Hour(this.hour+duration.hour, this.minutes+duration.minutes)
+}
